5ff6e96fed2234b6b1ffb9ea73e69d33456dcfc6
Softsquare <softsquare@t.local>
2016-03-28 22:00:11 +0700

    Initial Commit

---
+++ SupperCoolFristApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SupperCoolFristApp
+//
+//  Created by SS-NB-54-003 on 3/28/2559 BE.
+//  Copyright © 2559 XYZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
